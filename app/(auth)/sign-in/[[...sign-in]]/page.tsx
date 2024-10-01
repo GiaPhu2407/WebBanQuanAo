13177caf298954b64f11a4979ba64e14e8271dd5
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-10-01 23:11:51 +0700

    Shopping 1/10/2024

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
-
+export async function generateStaticParams() {
+  // Nếu bạn có tham số động, hãy tạo chúng ở đây
+  return [
+    { signUpParam: "param1" }, // Ví dụ tham số
+    { signUpParam: "param2" }, // Ví dụ tham số
+  ];
+}
 export default function Page() {
   return (
     <div className="flex justify-center items-center">
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
-
+export async function generateStaticParams() {
+  // Nếu bạn có tham số động, hãy tạo chúng ở đây
+  return [
+    { signUpParam: "param1" }, // Ví dụ tham số
+    { signUpParam: "param2" }, // Ví dụ tham số
+  ];
+}
 export default function Page() {
   return (
     <div className="flex justify-center items-center">
