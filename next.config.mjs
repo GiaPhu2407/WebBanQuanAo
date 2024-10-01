202013d92b6cd3e97f002c59866bc0663f3f1260
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-10-01 23:22:36 +0700

    Shopping 1/10/2024

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.js
+module.exports = {
+  output: "export", // hoặc cấu hình khác nếu cần
+  // các tùy chọn khác nếu có
+};
 
 export default nextConfig;
